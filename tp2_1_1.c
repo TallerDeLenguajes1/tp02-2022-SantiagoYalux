aa05380ba63e7097721891df4ae0c8454b703957
SantiagoYalux <santiyalux2@gmail.com>
2022-04-12 20:31:41 -0300

    commit tp2_1_1 arreglo de codigo c

---
+++ tp2_1_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 20
+int main()
+{
+    int i;
+    double vt[N];
+    srand(time(NULL));
+
+    for (i = 0; i < N; i++)
+    {
+        vt[i] = 1+rand()%100;
+        printf("%f   ", vt[i]);
+    }
+    
+
+}
